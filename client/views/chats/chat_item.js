71b6034448e3bddfdaef332a1f1eeb81e895aa63
Sai Prashanth <saiprashanth93@gmail.com>
2014-06-20 13:04:21 +0530

    added nothing

--- client/views/chats/chat_item.js
+++ client/views/chats/chat_item.js
@@ -3,7 +3,6 @@ Template.messageBox.events({
 		e.preventDefault();
 		var chat = {
 			message: $(e.target).find('[name=message]').val()
-			roomId: template.data._id
 
 		};
 		chat._id=Messages.insert(chat);
