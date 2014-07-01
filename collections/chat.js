56637fc8bb8d2ecce4ab76a717cfe674ce352385
yashrajchhabra <yashrajchhabra@gmail.com>
2014-07-01 15:38:14 +0530

    collection now called chats

--- collections/chat.js
+++ collections/chat.js
@@ -1,18 +1,20 @@
-Messages = new Meteor.Collection('chats');
+Chats = new Meteor.Collection('chats');
 Meteor.methods({
-chat: function(commentAttributes) {
-var user = Meteor.user();
-var room = Rooms.findOne(commentAttributes.roomId);
+	chat: function(commentAttributes) {
+		var user = Meteor.user();
+		var room = Rooms.findOne(commentAttributes.roomId);
 // ensure the user is logged in
+if (!user)
+	throw new Meteor.Error(401, "You need to login to make comments");
 if (!commentAttributes.body)
-throw new Meteor.Error(422, 'Please write some content');
+	throw new Meteor.Error(422, 'Please write some content');
 
 chat = _.extend(_.pick(commentAttributes, 'roomId', 'body'), {
-userId: user._id,
-author: user.username,
-submitted: new Date().getTime()
+	userId: user._id,
+	author: user.username,
+	submitted: new Date().getTime()
 });
-return Messages.insert(chat);
+return Chats.insert(chat);
 }
 });
 
