172d43b9dd9657dc6f13ba71224ad70441af089a
yashrajchhabra <yashrajchhabra@gmail.com>
2014-07-04 00:52:58 +0530

    added routes

--- client/views/rooms/room_list.js
+++ client/views/rooms/room_list.js
@@ -3,3 +3,12 @@ rooms: function() {
 return Rooms.find({}, {sort: {submitted: -1}});
 }
 });
+Template.roomsList.events({
+'submit form': function(e) {
+e.preventDefault();
+
+var searchString= $(e.target).find('[name=search]').val();
+var someCursor = Rooms.find({ title: searchString });
+Router.go('roomsList');
+}
+});
